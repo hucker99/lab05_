d98c6dc9c3bb9dfecce563880e02daf166141dfb
hucker99 <54907176+hucker99@users.noreply.github.com>
2020-02-14 15:14:27 +0300

    Update second.hpp

--- include/second.hpp
+++ include/second.hpp
@@ -1,7 +1,7 @@
 // Copyright 2018 You Name <your_email>
 
-#ifndef INCLUDE_second_HPP_
-#define INCLUDE_second_HPP_
+#ifndef INCLUDE_SECOND_HPP_
+#define INCLUDE_SECOND_HPP_
 
 #include <iostream>
 #define stack_size 10
@@ -9,11 +9,9 @@
 template <typename T>
 class Stack
 {
-    int64_t element_counter = stack_size-1;
-    
+    int64_t element_counter = stack_size-1; 
 public:
     T stack_contents[stack_size];
-    
     template <typename ... Args>
     void push_emplace(Args&&... value)
     {
@@ -21,23 +19,21 @@ public:
         stack_size[element_counter] = obj;
         element_counter--;
     }
-    
     void push(T&& value)
     {
         stack_size[element_counter] = value;
         element_counter--;
     }
-    
     T pop()
     {
         element_counter++;
         return stack_size[element_counter];
     }
-
     const T& head() const
     {
         return stack_size[element_counter+1];
     }
 };
+
 
 #endif // INCLUDE_HEADER_HPP_
