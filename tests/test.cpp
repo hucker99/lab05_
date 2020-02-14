8b52e007d219e3f22965b9cacc4c235d215cfaab
hucker99 <54907176+hucker99@users.noreply.github.com>
2020-02-14 14:11:57 +0300

    Update tests for first task

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,11 @@
 // Copyright 2018 Your Name <your_email>
 
 #include <gtest/gtest.h>
+#include "first.hpp"
 
 TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
+    stack <int> string;
+    int a = 12;
+    string.push(a);
+    EXPECT_EQ(string.head(), 12);
 }
