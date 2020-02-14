6005f54588c5a6764197c770f3dd23a50172c22f
hucker99 <54907176+hucker99@users.noreply.github.com>
2020-02-14 15:18:57 +0300

    Update first.hpp

--- include/first.hpp
+++ include/first.hpp
@@ -4,32 +4,32 @@
 #define INCLUDE_FIRST_HPP_
 
 #include <iostream>
-#define stack_size 10
+#define SIZE_OF_STACK 10
 
 template <typename T>
 class stack
 {
     int64_t element_counter = stack_size-1;
 public:
-    T stack_contents[stack_size];
+    T stack_contents[SIZE_OF_STACK];
     void push(const T& value)
     {
-        stack_size[element_counter] = value;
+        stack_contents[element_counter] = value;
         element_counter--;
     }
     void push(T&& value)
     {
-        stack_size[element_counter] = value;
+        stack_contents[element_counter] = value;
         element_counter--;
     }
     T pop()
     {
         element_counter++;
-        return stack_size[element_counter];
+        return stack_contents[element_counter];
     }
     const T& head() const
     {
-        return stack_size[element_counter+1];
+        return stack_contents[element_counter+1];
     }
 };
 
