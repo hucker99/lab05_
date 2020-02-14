b750f151c659218db18895535e7180062a5c133f
hucker99 <54907176+hucker99@users.noreply.github.com>
2020-02-14 14:53:24 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -8,4 +8,13 @@ TEST(Example, EmptyTest) {
     int a = 12;
     string.push(a);
     EXPECT_EQ(string.head(), 12);
+    Stack <std::string> some_string;
+    some_string.push("123");
+    some_string.push("456");
+    some_string.push("789");
+    EXPECT_EQ((std::string) x.pop(), "789");
+}
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
