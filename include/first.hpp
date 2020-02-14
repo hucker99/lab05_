8d51ec67ff264d62ad435ae322225ea49b4c8844
hucker99 <54907176+hucker99@users.noreply.github.com>
2020-02-14 13:31:35 +0300

    Update and Rename to "first"

---
+++ include/first.hpp
@@ -0,0 +1,41 @@
+// Copyright 2018 You Name <your_email>
+
+#ifndef INCLUDE_first_HPP_
+#define INCLUDE_first_HPP_
+
+#include <iostream>
+#define stack_size 10
+
+template <typename T>
+class stack
+{
+    int64_t element_counter=stack_size-1;
+    
+public:
+    T stack_contents[stack_size];
+    
+    void push(const T& value)
+    {
+        stack_size[element_counter]=value;
+        element_counter--;
+    }
+    
+    void push(T&& value)
+    {
+        stack_size[element_counter]=value;
+        element_counter--;
+    }
+    
+    T pop()
+    {
+        element_counter++;
+        return stack_size[element_counter];
+    }
+
+    const T& head() const
+    {
+        return stack_size[element_counter+1];
+    }
+};
+
+#endif // INCLUDE_HEADER_HPP_
--- include/header.hpp
+++
@@ -1,6 +0,0 @@
-// Copyright 2018 You Name <your_email>
-
-#ifndef INCLUDE_HEADER_HPP_
-#define INCLUDE_HEADER_HPP_
-
-#endif // INCLUDE_HEADER_HPP_
