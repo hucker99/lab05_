d248606c99085d094546bbe3e5a34606c771b600
hucker99 <54907176+hucker99@users.noreply.github.com>
2020-02-14 14:55:19 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -2,18 +2,24 @@
 
 #include <gtest/gtest.h>
 #include "first.hpp"
+#include "second.hpp"
 
 TEST(Example, EmptyTest) {
-    stack <int> string;
+    EXPECT_EQ(std::is_move_constructible<stack1<int>>::value, true);
+    EXPECT_EQ(std::is_move_assignable<stack1<int>>::value, true);
+    EXPECT_EQ(std::is_move_constructible<stack2<int>>::value, true);
+    EXPECT_EQ(std::is_move_assignable<stack2<int>>::value, true);
+    stack <int> number;
     int a = 12;
-    string.push(a);
-    EXPECT_EQ(string.head(), 12);
+    number.push(a);
+    EXPECT_EQ(number.head(), 12);
     Stack <std::string> some_string;
     some_string.push("123");
     some_string.push("456");
     some_string.push("789");
-    EXPECT_EQ((std::string) x.pop(), "789");
+    EXPECT_EQ((std::string) some_string.pop(), "789");
 }
+
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
